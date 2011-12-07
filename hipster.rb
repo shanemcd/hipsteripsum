e4a5e32d88c151e788c1988d3c1f07299f5fadc2
Shane McDonald <anatomyofashane@gmail.com>
2011-12-06 20:27:40 -0500

    initial commit

---
+++ hipster.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'json'
+
+class Hipster
+  def getText(paragraphs=2, type="centric")
+    response = Net::HTTP.get_response(URI.parse("http://hipsterjesus.com/api/?paras=" + paragraphs.to_s + "&type=hipster-" + type))
+    obj = JSON.parse(response.body)
+    return obj["text"]
+  end
+end
+
+h = Hipster.new
+
+puts h.getText(5, "centric")
